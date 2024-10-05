925c5bf56acab50483e898e24d5645caad6ae40c
Abhinav Pandey <pandeyabhinav56@gmail.com>
2024-10-05 11:26:54 +0530

    added waitgroup and mutex changs

--- 27goroutines/main.go
+++ 27goroutines/main.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"sync"
 	"time"
 )
 
+var signals = []string{"test"}
+
+var wg sync.WaitGroup
+var mutex sync.Mutex
+
 func main() {
 
-	go greeter("Hello")
-	greeter("Abhinav")
+	// go greeter("Hello")
+	// greeter("Abhinav")
+
+	websites := []string{
+		"http://github.com",
+		"http://go.dev",
+		"http://google.com",
+		"http://fb.com",
+		"http://instagram.com",
+	}
+
+	for _, w := range websites {
+		go getEndpoint(w)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+	fmt.Println(signals)
 }
 
 func greeter(s string) {
@@ -16,4 +39,19 @@ func greeter(s string) {
 		time.Sleep(3 * time.Millisecond) //this will wait for go routine thread to come and print
 		fmt.Println(s)
 	}
+}
+
+func getEndpoint(endpoint string) {
+	defer wg.Done()
+
+	result, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPS in http call")
+	}
+
+	mutex.Lock()
+	signals = append(signals, endpoint)
+	mutex.Unlock()
+
+	fmt.Printf("%v code and endpoint is: %v \n", result.StatusCode, endpoint)
 }
